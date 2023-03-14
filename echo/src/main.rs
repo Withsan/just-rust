8af75d1f6a4bd4ca1254b1f0131856bc3e5ba9bf
王彦霖 <wangyl@koal.com>
2023-03-14 14:21:26 +0800

    feat: add echo server

---
+++ echo/src/main.rs
@@ -0,0 +1,36 @@
+use std::error::Error;
+
+use tokio::io::{AsyncReadExt, AsyncWriteExt};
+use tokio::net::TcpListener;
+
+#[tokio::main]
+async fn main() -> Result<(), Box<dyn Error>> {
+    let listener = TcpListener::bind("127.0.0.1:8080").await?;
+    loop {
+        let (mut socket, addr) = listener.accept().await?;
+        println!("accept from {:?}", addr);
+        tokio::spawn(async move {
+            let mut buf = [0; 1024];
+            loop {
+                let n = match socket.read(&mut buf).await {
+                    Ok(n) if n == 0 => return,
+                    Ok(n) => n,
+                    Err(e) => {
+                        eprintln!("failed to read from socket; err={:?}", e);
+                        return;
+                    }
+                };
+                println!(
+                    "from client:[{:?}]",
+                    &buf.iter()
+                        .take(n)
+                        .map(|by| char::from(*by))
+                        .collect::<String>()
+                );
+                if let Err(e) = socket.write_all(&buf[0..n]).await {
+                    eprintln!("failed to write to socket; err={:?}", e);
+                }
+            }
+        });
+    }
+}
