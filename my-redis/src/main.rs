5018899090866244f2e9cc0fa8c85bc8944b004e
Withs <578935306@qq.com>
2023-03-15 23:26:25 +0800

    feat: play with mini-redis

--- my-redis/src/main.rs
+++ my-redis/src/main.rs
@@ -1,11 +1,20 @@
-use mini_redis::{client, Result};
+use mini_redis::{Connection, Frame, Result};
+use tokio::net::{TcpListener, TcpStream};
 
 #[tokio::main]
 async fn main() -> Result<()> {
-    let mut client = client::connect("127.0.0.1:6379").await?;
-    client.set("fuck", "you".into()).await?;
-    if let Some(result) = client.get("fuck").await? {
-        println!("got value from the mini-redis-server:{:?}", result);
+    let listener = TcpListener::bind("127.0.0.1:6379").await?;
+    loop {
+        let (stream, _client_addr) = listener.accept().await?;
+        process(stream).await?;
+    }
+}
+async fn process(stream: TcpStream) -> Result<()> {
+    let mut connection = Connection::new(stream);
+    if let Some(frame) = connection.read_frame().await? {
+        println!("Got: {:?}", frame);
+        let response = Frame::Error("fuck you too".to_string());
+        connection.write_frame(&response).await?;
     };
     Ok(())
 }
