2c63b4025e656d727fc13a62921e15ef53ce81b2
王彦霖 <wangyl@koal.com>
2023-03-15 14:09:29 +0800

    feat: learn mini-redis

---
+++ my-redis/src/main.rs
@@ -0,0 +1,11 @@
+use mini_redis::{client, Result};
+
+#[tokio::main]
+async fn main() -> Result<()> {
+    let mut client = client::connect("127.0.0.1:6379").await?;
+    client.set("fuck", "you".into()).await?;
+    if let Some(result) = client.get("fuck").await? {
+        println!("got value from the mini-redis-server:{:?}", result);
+    };
+    Ok(())
+}
