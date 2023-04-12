7be84a6c8257196b43c6101d5c6143f7a4240b30
王彦霖 <wangyl@koal.com>
2023-04-12 12:17:32 +0800

    feat: json

--- just-test/src/main.rs
+++ just-test/src/main.rs
@@ -1,32 +1,9 @@
-use std::error::Error;
-
-use tokio::fs::File;
-use tokio::io::{AsyncReadExt, AsyncWriteExt};
+use tokio_stream::StreamExt;
 
 #[tokio::main]
-async fn main() -> Result<(), Box<dyn Error>> {
-    let path = r"C:\Users\Withs\Desktop\heihei.md";
-    let content = String::from("hello rust");
-    create_file(path).await?;
-    write_to_file(path, content.into_bytes()).await?;
-    read_from_file(path).await?;
-    Ok(())
-}
-async fn create_file(path: &str) -> Result<(), Box<dyn Error>> {
-    let _file = File::create(path).await?;
-    Ok(())
-}
-async fn write_to_file(path: &str, content: Vec<u8>) -> Result<(), Box<dyn Error>> {
-    let mut file = File::open(path).await?;
-    println!("the content is :{:?}", content);
-    let n = file.write(&content[..]).await?;
-    println!("Wrote the first {} bytes", n);
-    Ok(())
-}
-async fn read_from_file(path: &str) -> Result<(), Box<dyn Error>> {
-    let mut file = File::open(path).await?;
-    let mut content = Vec::new();
-    let n = file.read_to_end(&mut content).await?;
-    println!("the bytes {:?}", n);
-    Ok(())
+async fn main() {
+    let mut iter = tokio_stream::iter(&[1, 23, 4, 5, 6]);
+    while let Some(item) = iter.next().await {
+        println!("it is {:?}", item);
+    }
 }
