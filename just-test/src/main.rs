62ba3238e8b712ebc8c1356c7202562c23883c25
王彦霖 <wangyl@koal.com>
2023-04-17 13:58:53 +0800

    feat: cather

--- just-test/src/main.rs
+++ just-test/src/main.rs
@@ -1,9 +1,35 @@
-use tokio_stream::StreamExt;
-
 #[tokio::main]
 async fn main() {
-    let mut iter = tokio_stream::iter(&[1, 23, 4, 5, 6]);
-    while let Some(item) = iter.next().await {
-        println!("it is {:?}", item);
+    let mut cacher = Cacher {
+        value: None,
+        query: |v| 5 + v,
+    };
+    println!("{:?}", cacher.value(11));
+}
+
+struct Cacher<F, T>
+where
+    F: Fn(T) -> T,
+{
+    value: Option<T>,
+    query: F,
+}
+
+impl<F, T> Cacher<F, T>
+where
+    F: Fn(T) -> T,
+{
+    fn value<'a>(&mut self, arg: T) -> T
+    where
+        T: 'a + Clone,
+    {
+        match &self.value {
+            Some(t) => t.clone(),
+            None => {
+                let v = (self.query)(arg);
+                self.value = Some(v.clone());
+                v
+            }
+        }
     }
 }
