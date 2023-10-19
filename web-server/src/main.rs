d48f432a540093cff005c57a38667cfcf165b1be
Lin <578935306@qq.com>
2023-10-19 15:06:13 +0800

    init and singlthread

---
+++ web-server/src/main.rs
@@ -0,0 +1,27 @@
+use std::{
+    fs,
+    io::{BufRead, BufReader, Write},
+    net::{TcpListener, TcpStream},
+};
+
+fn main() {
+    let tcp_listenr = TcpListener::bind("127.0.0.1:1993").unwrap();
+    for stream in tcp_listenr.incoming() {
+        let stream = stream.unwrap();
+        println!("Connection established!");
+        handle_connection(stream);
+    }
+}
+fn handle_connection(mut stream: TcpStream) {
+    let buf_reader = BufReader::new(&mut stream);
+    let request_line = buf_reader.lines().next().unwrap().unwrap();
+    let (status_line, filename) = if request_line == "GET / HTTP/1.1" {
+        ("HTTP/1.1 200 OK", "hello.html")
+    } else {
+        ("HTTP/1.1 404 NOT FOUND", "404.html")
+    };
+    let contents = fs::read_to_string(filename).unwrap();
+    let length = contents.len();
+    let resopnse = format!("{status_line}\r\nContent-Length: {length}\r\n\r\n{contents}");
+    stream.write_all(resopnse.as_bytes()).unwrap();
+}
