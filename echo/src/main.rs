3c21d4526e743b94e6c0758b0a230310f138d3b0
Lin <578935306@qq.com>
2023-12-22 13:01:26 +0800

    add from which server infomation

--- echo/src/main.rs
+++ echo/src/main.rs
@@ -1,23 +1,29 @@
 use std::env;
 use std::error::Error;
-
 use tokio::io::{AsyncReadExt, AsyncWriteExt};
 use tokio::net::TcpListener;
 
 #[tokio::main]
 async fn main() -> Result<(), Box<dyn Error>> {
     let mut args = env::args();
-    let boxed_port = args.next().unwrap().into_boxed_str();
+    let boxed_port = args.nth(1).unwrap().into_boxed_str();
     let port: &'static str = Box::leak(boxed_port);
-    let listener = TcpListener::bind("127.0.0.1:8081").await?;
+    let mut server_addr = String::from("127.0.0.1:");
+    server_addr.push_str(&port);
+    println!("Echo server listen on {}", &server_addr);
+    let listener = TcpListener::bind(server_addr.clone()).await?;
+    let server_addr: &'static str = Box::leak(server_addr.into_boxed_str());
     loop {
         let (mut socket, addr) = listener.accept().await?;
         println!("accept from {:?}", addr);
         tokio::spawn(async move {
-            let mut buf = vec![];
+            let mut buf = vec![0; 1024];
             loop {
                 let n = match socket.read(&mut buf).await {
-                    Ok(n) if n == 0 => return,
+                    Ok(n) if n == 0 => {
+                        println!("read zero byte!");
+                        return;
+                    }
                     Ok(n) => n,
                     Err(e) => {
                         eprintln!("failed to read from socket; err={:?}", e);
@@ -28,11 +34,16 @@ async fn main() -> Result<(), Box<dyn Error>> {
                     "from client:[{:?}]",
                     &buf.iter()
                         .take(n)
-                        .chain(&port.as_bytes())
                         .map(|by| char::from(*by))
                         .collect::<String>()
                 );
-                if let Err(e) = socket.write_all(&buf[0..n]).await {
+
+                let mut from_str = String::from(" from: ");
+                from_str.push_str(&server_addr.to_string().as_str());
+                if let Err(e) = socket
+                    .write_all(&[&buf[0..n], &from_str.as_str().as_bytes()[..]].concat()[..])
+                    .await
+                {
                     eprintln!("failed to write to socket; err={:?}", e);
                 }
             }
