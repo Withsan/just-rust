812e39f9b5b5be470b08696bbabf94d8b7fe9366
Lin <578935306@qq.com>
2023-12-27 12:57:52 +0800

    use tracing

--- echo/src/main.rs
+++ echo/src/main.rs
@@ -2,35 +2,40 @@ use std::env;
 use std::error::Error;
 use tokio::io::{AsyncReadExt, AsyncWriteExt};
 use tokio::net::TcpListener;
+use tracing::Level;
 
 #[tokio::main]
 async fn main() -> Result<(), Box<dyn Error>> {
+    let subscribe = tracing_subscriber::FmtSubscriber::builder()
+        .with_max_level(Level::INFO)
+        .finish();
+    tracing::subscriber::set_global_default(subscribe).expect("error");
     let mut args = env::args();
     let boxed_port = args.nth(1).unwrap().into_boxed_str();
     let port: &'static str = Box::leak(boxed_port);
     let mut server_addr = String::from("127.0.0.1:");
     server_addr.push_str(&port);
-    println!("Echo server listen on {}", &server_addr);
+    tracing::info!("Echo server listen on {}", &server_addr);
     let listener = TcpListener::bind(server_addr.clone()).await?;
     let server_addr: &'static str = Box::leak(server_addr.into_boxed_str());
     loop {
         let (mut socket, addr) = listener.accept().await?;
-        println!("accept from {:?}", addr);
+        tracing::info!("accept from {:?}", addr);
         tokio::spawn(async move {
             let mut buf = vec![0; 1024];
             loop {
                 let n = match socket.read(&mut buf).await {
                     Ok(n) if n == 0 => {
-                        println!("read zero byte!");
+                        tracing::info!("read zero byte!");
                         return;
                     }
                     Ok(n) => n,
                     Err(e) => {
-                        eprintln!("failed to read from socket; err={:?}", e);
+                        tracing::error!("failed to read from socket; err={:?}", e);
                         return;
                     }
                 };
-                println!(
+                tracing::info!(
                     "from client:[{:?}]",
                     &buf.iter()
                         .take(n)
@@ -44,7 +49,7 @@ async fn main() -> Result<(), Box<dyn Error>> {
                     .write_all(&[&buf[0..n], &from_str.as_str().as_bytes()[..]].concat()[..])
                     .await
                 {
-                    eprintln!("failed to write to socket; err={:?}", e);
+                    tracing::error!("failed to write to socket; err={:?}", e);
                 }
             }
         });
